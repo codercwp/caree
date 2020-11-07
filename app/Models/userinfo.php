e59617e07fa5550d265827faa88a5ad6b7366868
codercwp <2088134710@qq.com>
2020-11-07 18:47:07 +0800

    project init

---
+++ app/Models/userinfo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class userinfo extends Model
+{
+    protected $table = "userinfo";
+    public $timestamps = true;
+    protected $guarded = [];
+}
